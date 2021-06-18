d4ad0236bc04684f3c825bee0d312bb4d6582954
Chris <chrisfan@jtechdigital.com>
2021-06-17 20:33:45 -0500

    feat: add cracro package, setting @ for for src folder path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import './assets/css/reset.css';
+import '@/assets/css/reset.css';
 
 ReactDOM.render(
   <App />,
